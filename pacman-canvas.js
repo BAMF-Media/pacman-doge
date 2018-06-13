9c09f99bf816a0bfe4e132a6a23d1900e7637703
Nathan Glass <nathanglass@nathans-air.lan>
2018-06-13 16:39:34 -0700

    background looking good

--- pacman-canvas.js
+++ pacman-canvas.js
@@ -393,64 +393,64 @@ function geronimo() {
 
 			// buildWall(context_walls, 0, 0, 6, 1)
 			// //horizontal outer
-			buildWall(context_walls,0,0,18,1);
-			buildWall(context_walls,0,12,18,1);
+			// buildWall(context_walls,0,0,18,1);
+			// buildWall(context_walls,0,12,18,1);
 			
-			// vertical outer
-			buildWall(context_walls,0,0,1,6);
-			buildWall(context_walls,0,7,1,6);
-			buildWall(context_walls,17,0,1,6);
-			buildWall(context_walls,17,7,1,6);
+			// // vertical outer
+			// buildWall(context_walls,0,0,1,6);
+			// buildWall(context_walls,0,7,1,6);
+			// buildWall(context_walls,17,0,1,6);
+			// buildWall(context_walls,17,7,1,6);
 			
-			// ghost base
-			buildWall(context_walls,7,4,1,1);
-			buildWall(context_walls,6,5,1,2);
-			buildWall(context_walls,10,4,1,1);
-			buildWall(context_walls,11,5,1,2);
-			buildWall(context_walls,6,6,6,1);
+			// // ghost base
+			// buildWall(context_walls,7,4,1,1);
+			// buildWall(context_walls,6,5,1,2);
+			// buildWall(context_walls,10,4,1,1);
+			// buildWall(context_walls,11,5,1,2);
+			// buildWall(context_walls,6,6,6,1);
 			
 			// ghost base door
 			context_walls.fillRect(8*2*pacman.radius,pacman.radius/2+4*2*pacman.radius+5, 4*pacman.radius, 1);
 			
 			// single blocks
-			buildWall(context_walls,4,0,1,2);
-			buildWall(context_walls,13,0,1,2);
+			// buildWall(context_walls,4,0,1,2);
+			// buildWall(context_walls,13,0,1,2);
 			
-			buildWall(context_walls,2,2,1,2);
-			buildWall(context_walls,6,2,2,1);
-			buildWall(context_walls,15,2,1,2);
-			buildWall(context_walls,10,2,2,1);
+			// buildWall(context_walls,2,2,1,2);
+			// buildWall(context_walls,6,2,2,1);
+			// buildWall(context_walls,15,2,1,2);
+			// buildWall(context_walls,10,2,2,1);
 			
-			buildWall(context_walls,2,3,2,1);
-			buildWall(context_walls,14,3,2,1);
-			buildWall(context_walls,5,3,1,1);
-			buildWall(context_walls,12,3,1,1);
-			buildWall(context_walls,3,3,1,3);
-			buildWall(context_walls,14,3,1,3);
+			// buildWall(context_walls,2,3,2,1);
+			// buildWall(context_walls,14,3,2,1);
+			// buildWall(context_walls,5,3,1,1);
+			// buildWall(context_walls,12,3,1,1);
+			// buildWall(context_walls,3,3,1,3);
+			// buildWall(context_walls,14,3,1,3);
 			
-			buildWall(context_walls,3,4,1,1);
-			buildWall(context_walls,14,4,1,1);
+			// buildWall(context_walls,3,4,1,1);
+			// buildWall(context_walls,14,4,1,1);
 			
-			buildWall(context_walls,0,5,2,1);
-			buildWall(context_walls,3,5,2,1);
-			buildWall(context_walls,16,5,2,1);
-			buildWall(context_walls,13,5,2,1);
+			// buildWall(context_walls,0,5,2,1);
+			// buildWall(context_walls,3,5,2,1);
+			// buildWall(context_walls,16,5,2,1);
+			// buildWall(context_walls,13,5,2,1);
 			
-			buildWall(context_walls,0,7,2,2);
-			buildWall(context_walls,16,7,2,2);
-			buildWall(context_walls,3,7,2,2);
-			buildWall(context_walls,13,7,2,2);
+			// buildWall(context_walls,0,7,2,2);
+			// buildWall(context_walls,16,7,2,2);
+			// buildWall(context_walls,3,7,2,2);
+			// buildWall(context_walls,13,7,2,2);
 			
-			buildWall(context_walls,4,8,2,2);
-			buildWall(context_walls,12,8,2,2);
-			buildWall(context_walls,5,8,3,1);
-			buildWall(context_walls,10,8,3,1);
+			// buildWall(context_walls,4,8,2,2);
+			// buildWall(context_walls,12,8,2,2);
+			// buildWall(context_walls,5,8,3,1);
+			// buildWall(context_walls,10,8,3,1);
 			
-			buildWall(context_walls,2,10,1,1);
-			buildWall(context_walls,15,10,1,1);
-			buildWall(context_walls,7,10,4,1);
-			buildWall(context_walls,4,11,2,2);
-			buildWall(context_walls,12,11,2,2);
+			// buildWall(context_walls,2,10,1,1);
+			// buildWall(context_walls,15,10,1,1);
+			// buildWall(context_walls,7,10,4,1);
+			// buildWall(context_walls,4,11,2,2);
+			// buildWall(context_walls,12,11,2,2);
 			/* ------------ End Pre-Build Walls  ------------ */
 		};
 
@@ -1379,7 +1379,7 @@ function checkAppCache() {
 			context.beginPath();
 			context.fillStyle = "White";
 			context.strokeStyle = "White";
-			context.globalCompositeOperation='destination-over';
+			// context.globalCompositeOperation='destination-over';
 			
 			var dotPosY;
 			$.each(game.map.posY, function(i, item) {
